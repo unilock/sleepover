60ae3ce646d3a78c8b845a637a80f60b11b225ad
unilock <unilock@fennet.rentals>
2022-12-09 10:41:47 -0500

    here we go!!
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/net/unilock/sleepover/mixin/ExampleMixin.java
+++ src/main/java/net/unilock/sleepover/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package net.unilock.sleepover.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.unilock.sleepover.Sleepover;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		Sleepover.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
