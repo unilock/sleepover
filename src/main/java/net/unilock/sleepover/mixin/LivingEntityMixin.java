45c275fdb762114c5ea1ff2d8a4cc82842dc64df
unilock <unilock@fennet.rentals>
2024-01-14 15:06:02 -0500

    Simplify mod drastically
    making use of all i have learned
    
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/net/unilock/sleepover/mixin/LivingEntityMixin.java
+++ src/main/java/net/unilock/sleepover/mixin/LivingEntityMixin.java
@@ -1,84 +1,24 @@
 package net.unilock.sleepover.mixin;
 
-import net.minecraft.block.BedBlock;
-import net.minecraft.block.Block;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.block.BlockState;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.List;
-import java.util.Optional;
-
-// REDUNDANT CODE:
-// - posBed = pos
-// - worldBed = world
-// - ^^^ defined after setVars()
-// - ijava.util.Objects.requireNonNull(world); - (idk what this does)
-// - posBed.filter(worldBed::isChunkLoaded).ifPresent((pos) -> {...}) - i guess this checks
-// 																	    whether the block is
-// 																	    chunkloaded?
-// - BlockState blockState = this.world.getBlockState(pos) - can't avoid
-// - if (blockState.getBlock() instanceof BedBlock) - can't avoid
 
 @Mixin(LivingEntity.class)
-public abstract class LivingEntityMixin extends Entity {
-	public LivingEntityMixin(EntityType<?> type, World world) {
-		super(type, world);
-	}
+public abstract class LivingEntityMixin {
+	@WrapOperation(method = "method_18404", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z"))
+	private boolean stayOccupiedIfPlayer(World instance, BlockPos pos, BlockState state, int flags, Operation<Boolean> original) {
+		if (!instance.getEntitiesByClass(PlayerEntity.class, new Box(pos), LivingEntity::isSleeping).isEmpty()) {
+			return true;
+		}
 
-	Optional<BlockPos> posBed;
-	World worldBed;
-
-	@Shadow
-	public abstract Optional<BlockPos> getSleepingPosition();
-
-	@Inject(method = "wakeUp", at = @At("HEAD"))
-	private void setVars(CallbackInfo ci) {
-		posBed = this.getSleepingPosition();
-		worldBed = this.world;
-	}
-
-	@Inject(method = "wakeUp", at = @At("TAIL"))
-	private void stayOccupiedIfPlayer(CallbackInfo ci) {
-		System.out.println("[?] posBed = " + posBed);
-		System.out.println("[?] world = " + worldBed);
-
-		java.util.Objects.requireNonNull(world);
-
-		System.out.println("[?] is block @ posBed chunkloaded?");
-		posBed.filter(worldBed::isChunkLoaded).ifPresent((pos) -> {
-			System.out.println("[?] yes!!");
-
-			BlockState blockState = this.world.getBlockState(pos);
-			System.out.println("[?] blockState = " + blockState);
-
-			System.out.println("[?] is block @ posBed a bed? (hopefully!)");
-			if (blockState.getBlock() instanceof BedBlock) {
-				System.out.println("[?] yes!!");
-
-				System.out.println("[?] is there a sleeping player @ posBed?");
-				List<PlayerEntity> list = world.getEntitiesByClass(PlayerEntity.class, new Box(pos), LivingEntity::isSleeping);
-				if (!list.isEmpty()) {
-					System.out.println("[?] yes!!");
-
-					System.out.println("[?] set occupied = true");
-					this.world.setBlockState(pos, (BlockState) blockState.with(BedBlock.OCCUPIED, true), Block.NOTIFY_ALL);
-				} else {
-					System.out.println("[?] no!?");
-				}
-			} else {
-				System.out.println("[?] no!?");
-			}
-		});
+		return original.call(instance, pos, state, flags);
 	}
 }
